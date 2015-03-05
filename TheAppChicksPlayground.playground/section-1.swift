3f6e59eef0dfa0943dcfc9e022efd4cb09582253
Carey McLean <carey.mclean@vsp.com>
2015-03-05 15:36:02 -0800

    Initial commit for The App Chicks playground to check swift out and play!

---
+++ TheAppChicksPlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+// Add comments to explain value to your code.. 
+// Other people need to be able to understand what you have written!  :-)
+// .. and you in a later life.. ;-)
+//
+// http://TheAppChicks.com
+// http://TeedOff.com
+
+import UIKit
+
+var str = "Hello, playground"
