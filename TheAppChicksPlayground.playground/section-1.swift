b00b53df2699b99293b4c3cb7875c368b2a15af2
Carey McLean <carey@TheAppChicks.com>
2015-03-05 15:53:01 -0800

    Playing with string variables and concatenation

--- TheAppChicksPlayground.playground/section-1.swift
+++ TheAppChicksPlayground.playground/section-1.swift
@@ -9,3 +9,24 @@
 import UIKit
 
 var str = "Hello, playground"
+//reassign variable str by not putting 'var' in front of it
+str = "Hello World"
+str
+
+//specify variable names in lowercase
+var language = "Swift"
+
+// use camel casing in programming, capitalize the 2nd word
+var programmingLanguage = "Swift"
+
+// 3 words.. capitalize subsequent words... increases readability for yourself and others
+var modernProgrammingLanguage = "Swift"
+
+// concatenate a string literal and another variable
+var greeting = "Hello " + modernProgrammingLanguage
+
+// use two variables to create a brand new string
+var str2 = "Hello "
+var greeting2 = str2 + modernProgrammingLanguage
+
+
