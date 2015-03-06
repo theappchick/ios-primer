203ee77f222c8c1ba69199ea79d1902e18984a79
Carey McLean <carey@TheAppChicks.com>
2015-03-05 17:25:14 -0800

    Converting Fahrenheit to Celsius in Swift

--- TheAppChicksNumbers.playground/section-1.swift
+++ TheAppChicksNumbers.playground/section-1.swift
@@ -18,3 +18,13 @@ var version : Double = 1.0 // Double = 15 decimal digits and Float = 7 decimal d
 var isAwesome : Bool = true // (can have 1 or 0) Boolean or Bool, can be true or false
 
 //covered 3 new types of numbers, int, double, bool
+
+
+// Extra Credit
+// Using the math operators taught in this stage create a formula to change any given temperature from Fahrenheit to Celsius.
+
+// Convert 68 degrees Fahrenheit to degrees Celsius:
+// T(°C) = (68°F - 32) × 5/9 = 20 °C
+
+var fahrenheit = 68
+let fahrenheitToCelsius = (fahrenheit - 32) * 5/9
