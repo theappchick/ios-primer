3de4e4458f986917afbffe78375b7312f536e805
Carey McLean <carey@TheAppChicks.com>
2015-03-05 16:44:15 -0800

    Numbers and Bool in Swift

---
+++ TheAppChicksNumbers.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+// Add comments to explain value to your code..
+// Other people need to be able to understand what you have written!  :-)
+// .. and you in a later life.. ;-)
+//
+// http://TheAppChicks.com
+// http://TeedOff.com
+// http://Drinktography.com
+
+import UIKit
+
+let language = "Swift"
+
+let year : Int = 2014 //Int or Integer (used for whole numbers, no decimal places)
+
+var version : Double = 1.0 // Double = 15 decimal digits and Float = 7 decimal digits (Double is defaut if implicitly defined)
+
+var isAwesome : Bool = true // (can have 1 or 0) Boolean or Bool, can be true or false
+
+//covered 3 new types of numbers, int, double, bool
