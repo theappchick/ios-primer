37b2a5c2b7cc14a5b633e732ee7fecc8a2beb2c0
Carey McLean <carey@TheAppChicks.com>
2015-03-05 16:13:53 -0800

    Playing with variables and constants

--- TheAppChicksPlayground.playground/section-1.swift
+++ TheAppChicksPlayground.playground/section-1.swift
@@ -19,14 +19,21 @@ var language = "Swift"
 // use camel casing in programming, capitalize the 2nd word
 var programmingLanguage = "Swift"
 
+
+// Constants are similar to variables as they too are used to store information. 
+// However, as their name states they remain constant which means their value cannot be changed.
+
 // 3 words.. capitalize subsequent words... increases readability for yourself and others
-var modernProgrammingLanguage = "Swift"
+// change var to let.. now it's a constant
+let modernProgrammingLanguage = "Swift"
+//this is illegal.. uncomment and review the error..
+//modernProgrammingLanguage = "Objective-C"
 
 // concatenate a string literal and another variable
 var greeting = "Hello " + modernProgrammingLanguage
 
 // use two variables to create a brand new string
-var str2 = "Hello "
-var greeting2 = str2 + modernProgrammingLanguage
-
-
+// using type to declare type of variable you are defining
+var str2 : String = "Hello "
+// known as an implicit or inferred type of the type is not specified
+var greeting2 : String = str2 + modernProgrammingLanguage
